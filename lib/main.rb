121fc96502313b5af5c292d657410e1e682fb6fd
curly08 <mattynoakes08@gmail.com>
2021-09-29 00:28:25 -0400

    Add word_list and secret_word variables

--- lib/main.rb
+++ lib/main.rb
@@ -1,15 +1,18 @@
-word_list = File.read('5desk.txt')
+# frozen_string_literal: true
 
+# Game class
 class Game
   attr_accessor :display
-  attr_reader :secret_word
+  attr_reader :secret_word, :word_list
 
   def initialize
-    @secret_word = word_list.split.sample
+    @word_list = File.read('5desk.txt').split.select { |word| word if word.length.between?(5, 12) }
+    @secret_word = word_list.sample
     @display = String.new.ljust(secret_word.length, '_')
   end
 end
 
+# Player class
 class Player
   def initialize
     puts 'What is your name?'
@@ -20,3 +23,5 @@ end
 def choose_letter
   letter = gets.chomp
 end
+
+Game.new
