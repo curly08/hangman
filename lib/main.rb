e1e1a84a8efc1a72807fce0e514b869b9b90827f
curly08 <mattynoakes08@gmail.com>
2021-09-29 22:07:27 -0400

    Add play_game, play_turn, correct_guess?, and check_for_letter methods

--- lib/main.rb
+++ lib/main.rb
@@ -4,14 +4,55 @@ require 'pry-byebug'
 
 # Game class
 class Game
-  attr_accessor :display
-  attr_reader :secret_word, :word_list
+  attr_accessor :display, :game_over, :wrong_guesses
+  attr_reader :secret_word, :word_list, :player, :max_wrong_guesses
 
-  def initialize
+  def initialize(player)
     @word_list = File.read('5desk.txt').split.select { |word| word if word.length.between?(5, 12) }
     @secret_word = word_list.sample
-    @display = String.new.ljust(secret_word.length, '_')
+    @player = player
+    @display = Array.new(secret_word.length, '_')
+    @max_wrong_guesses = 6
+    @wrong_guesses = 0
     @game_over = false
+  end
+
+  def play_game
+    play_turn while @game_over == false
+  end
+
+  def play_turn
+    puts secret_word
+    puts display.join(' ')
+    input = player.choose_letter
+    correct_guess?(input) if input.length > 1
+    check_for_letter(input) if input.length == 1
+    return unless @wrong_guesses == @max_wrong_guesses
+
+    @game_over = true
+    puts 'You lose because you ran out of guesses.'
+  end
+
+  def correct_guess?(guess)
+    if guess == secret_word
+      @game_over = true
+      puts 'Congrats! You won!'
+    else
+      @wrong_guesses += 1
+      puts 'Incorrect'
+    end
+  end
+
+  def check_for_letter(letter)
+    if secret_word.downcase.include?(letter)
+      secret_word.split('').each_with_index do |v, i|
+        display[i] = v if v.casecmp?(letter)
+      end
+      puts "Yes! The word includes #{letter}"
+    else
+      @wrong_guesses += 1
+      puts "Sorry, the word does not include \"#{letter}\""
+    end
   end
 end
 
@@ -19,17 +60,17 @@ end
 class Player
   def initialize(name)
     @name = name
-    @guessed_letters = []
+    @guesses = []
   end
 
   def choose_letter
-    puts 'Guess a letter!'
+    puts 'Pick a letter or guess the word!'
     begin
       input = gets.chomp.downcase
-      raise 'Invalid input! Type a letter.' unless input.match?(/^[a-z]+$/i) && input.length == 1
-      raise 'You have already guessed that letter!' unless @guessed_letters.none?(input)
+      raise 'Invalid input! Type a letter or guess the word.' unless input.match?(/^[a-z]+$/i)
+      raise 'You have already guessed that! Try something else.' unless @guesses.none?(input)
 
-      @guessed_letters.push(input)
+      @guesses.push(input)
       input
     rescue StandardError => e
       puts e.to_s
@@ -38,16 +79,5 @@ class Player
   end
 end
 
-# def store_guess(guess)
-#   puts guessed_letters
-# end
-
-
-# def compare_guess(letter, secret_word)
-#   if secret_word.include?(letter) do
-    
-#   end
-# end
-
-# Game.new
-matt = Player.new('matt')
+new_game = Game.new(Player.new('matt'))
+new_game.play_game
